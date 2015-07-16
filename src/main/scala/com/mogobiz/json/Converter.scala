8d8d407be9e4e03fe372b2618d15d645065d2a42
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-07-16 07:00:54 +0200

    add copyright notice

--- src/main/scala/com/mogobiz/json/Converter.scala
+++ src/main/scala/com/mogobiz/json/Converter.scala
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015 Mogobiz SARL. All rights reserved.
+ */
+
 package com.mogobiz.json
 
 import java.io.{BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
